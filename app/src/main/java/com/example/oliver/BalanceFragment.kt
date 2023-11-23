da80dacc03a950c3f771c83e6f9419646661a344
GABU <gabrielngeti4@gmail.com>
2023-11-23 13:07:26 +0300

    added balance section

--- app/src/main/java/com/example/oliver/BalanceFragment.kt
+++ app/src/main/java/com/example/oliver/BalanceFragment.kt
@@ -8,9 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
 
 class BalanceFragment : Fragment() {
 
@@ -18,6 +16,7 @@ class BalanceFragment : Fragment() {
     private lateinit var fertilizerPriceTextView: TextView
     private lateinit var systemMaintenanceTextView: TextView
     private lateinit var annualFeeTextView: TextView
+    private lateinit var totalBalanceTextView: TextView
 
     private lateinit var auth: FirebaseAuth
     private lateinit var currentUser: FirebaseUser
@@ -28,11 +27,10 @@ class BalanceFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
         currentUser = auth.currentUser!!
-
         database = FirebaseDatabase.getInstance()
-        produceDatabaseRef = database.reference.child("products")
+        produceDatabaseRef = database.reference.child("produce")
         ordersDatabaseRef = database.reference.child("orders")
     }
 
@@ -46,6 +44,7 @@ class BalanceFragment : Fragment() {
         fertilizerPriceTextView = view.findViewById(R.id.fertilizerPrice)
         systemMaintenanceTextView = view.findViewById(R.id.systemMaintanance)
         annualFeeTextView = view.findViewById(R.id.annualFee)
+        totalBalanceTextView = view.findViewById(R.id.totalBalance)
 
         calculateProduceReturns()
         fetchFertilizerPrice()
@@ -62,11 +61,13 @@ class BalanceFragment : Fragment() {
                     var totalProduceReturns = 0
 
                     for (produceSnapShot in snapshot.children) {
-                        val produceAmount = produceSnapShot.child("amount").getValue(Int::class.java) ?: 0
+                        val produceAmount =
+                            produceSnapShot.child("amount").getValue(Int::class.java) ?: 0
                         totalProduceReturns += produceAmount
                     }
 
                     produceReturnsTextView.text = "Produce Return: $totalProduceReturns"
+                    calculateBalance(totalProduceReturns)
                 }
 
                 override fun onCancelled(error: DatabaseError) {
@@ -82,11 +83,13 @@ class BalanceFragment : Fragment() {
                     var totalFertilizerPrice = 0
 
                     for (orderSnapShot in snapshot.children) {
-                        val orderAmount = orderSnapShot.child("amount").getValue(Int::class.java) ?: 0
+                        val orderAmount =
+                            orderSnapShot.child("amount").getValue(Int::class.java) ?: 0
                         totalFertilizerPrice += orderAmount
                     }
 
                     fertilizerPriceTextView.text = "Fertilizer Price: $totalFertilizerPrice"
+                    calculateBalance()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
@@ -96,10 +99,22 @@ class BalanceFragment : Fragment() {
     }
 
     private fun setSystemMaintenanceAmount() {
-        systemMaintenanceTextView.text = "System Maintenance: 2000"
+        val systemMaintenanceAmount = 2000
+        systemMaintenanceTextView.text = "System Maintenance: $systemMaintenanceAmount"
+        calculateBalance()
     }
 
     private fun setAnnualFeeAmount() {
-        annualFeeTextView.text = "Annual Fee: 500"
+        val annualFeeAmount = 500
+        annualFeeTextView.text = "Annual Fee: $annualFeeAmount"
+        calculateBalance()
+    }
+
+    private fun calculateBalance(produceReturns: Int = 0, fertilizerPrice: Int = 0) {
+        val systemMaintenanceAmount = 2000
+        val annualFeeAmount = 500
+
+        val totalBalance = produceReturns - (fertilizerPrice + systemMaintenanceAmount + annualFeeAmount)
+        totalBalanceTextView.text = "Balance: $totalBalance"
     }
 }
