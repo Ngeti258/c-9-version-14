e2aa25ab4f8896752b02ce9e63a8eb87842fac9a
GABU <gabrielngeti4@gmail.com>
2023-11-23 12:30:52 +0300

    updated it to improve aestetics

--- app/src/main/java/com/example/oliver/HistoryAdapter.kt
+++ app/src/main/java/com/example/oliver/HistoryAdapter.kt
@@ -1,49 +1,48 @@
 package com.example.oliver
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class HistoryAdapter(private val productList: List<Product>) : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val produceDetails: TextView = itemView.findViewById(R.id.produceDetails)
+        val receiveTime: TextView = itemView.findViewById(R.id.receiveTime)
 
 
-class HistoryAdapter(private val context: Context, var productList: MutableList<HistoryFragment.OrderHistory>) :
-    RecyclerView.Adapter<HistoryAdapter.ProductViewHolder>() {
-
-    private var onItemClickListener: OnItemClickListener = object : OnItemClickListener {
-        override fun onItemClick(product: HistoryFragment.OrderHistory) {
-
-        }
     }
 
-    interface OnItemClickListener {
-        fun onItemClick(product: HistoryFragment.OrderHistory)
+    // Create new ViewHolders (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.history_layout, parent, false)
+        return ViewHolder(view)
     }
 
-    fun setOnItemClickListener(listener: OnItemClickListener) {
-        onItemClickListener = listener
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.history_layout, parent, false)
-        return ProductViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+    // Replace the contents of a ViewHolder (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val product = productList[position]
 
-        holder.fertilizerTypeTV.text = product.coffeeType
-        holder.fertilizerQuantityTV.text = " ${product.quantity} kilograms"
+        holder.produceDetails.text = " ${product.quantity}kgs for {${product.amount}}"
+        holder.receiveTime.text = " ${product.timestamp?.let { formatTimestamp(it) }}"
+
 
 
     }
 
-    override fun getItemCount() = productList.size
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        return productList.size
+    }
 
-    inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val fertilizerTypeTV: TextView = itemView.findViewById(R.id.fertilizerTypeTV)
-        val fertilizerQuantityTV: TextView = itemView.findViewById(R.id.fertilizerQuantityTV)
-
+    // Helper function to format timestamp to dd-mm-yyyy
+    private fun formatTimestamp(timestamp: Long): String {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val date = Date(timestamp)
+        return dateFormat.format(date)
     }
 }
