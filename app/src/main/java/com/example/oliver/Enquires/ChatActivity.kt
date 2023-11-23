b757e82794485d976973494470b8c503bf79497b
GABU <gabrielngeti4@gmail.com>
2023-11-23 12:26:44 +0300

    facilitate enquiries

--- app/src/main/java/com/example/oliver/Enquires/ChatActivity.kt
+++ app/src/main/java/com/example/oliver/Enquires/ChatActivity.kt
@@ -1,16 +1,16 @@
 package com.example.oliver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.oliver.Enquires.MessageAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var chatRecyclerView: RecyclerView
@@ -19,10 +19,6 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
     private lateinit var mDbRef:DatabaseReference
-
-    val receiverRoom:String?=null
-    val senderRoom:String?=null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,23 +65,17 @@ class ChatActivity : AppCompatActivity() {
 
 
         //adding message to the database
-        sendButton.setOnClickListener {
+        sendButton.setOnClickListener{
             val message = messageBox.text.toString()
+            val messageObject = Message(message, senderUid)
 
-            if (message.isNotEmpty()) {
-                val messageObject = Message(message, senderUid)
-
-                mDbRef.child("chats").child(senderRoom!!).child("messages").push()
-                    .setValue(messageObject).addOnSuccessListener {
-                        mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
-                            .setValue(messageObject)
-                    }
-                messageBox.setText("")
-            } else {
-                Toast.makeText(this, "Message cannot be empty", Toast.LENGTH_SHORT).show()
-            }
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+            messageBox.setText("")
         }
-
 
     }
 }
